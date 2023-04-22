7c015ce0b316fc01823be898b5fa22673918c932
joystellaadaobi <joystellaada@gmail.com>
2023-04-22 15:56:12 -0700

    gcc 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - main entry point
+ *main - entry point
  *
- *returm : always 0 (success)
+ *return : Always 0 (sucess)
  */
 int main(void)
 {
-	puts(*"Programming is like building a multilingual puzzle\n");
+	puts(*Programming is like building a multilingual puzzle*);
 	return (0);
 }
