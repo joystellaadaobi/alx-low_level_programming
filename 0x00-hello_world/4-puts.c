66a238b138b7f80ff9f0cafc7c5215b2aa91546a
joystellaadaobi <joystellaada@gmail.com>
2023-04-23 14:11:46 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry point
  *
- *return : Always 0 (sucess)
+ *Return : Always 0 (sucess)
  */
 int main(void)
 {
-	puts(*Programming is like building a multilingual puzzle*);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
